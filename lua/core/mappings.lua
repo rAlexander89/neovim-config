97055fbdd50e622c45d4de7b86276bc4309700ea
rAlexander89 <ryanalexanderacosta@icloud.com>
2024-11-11 13:36:31 -0800

    floating reference

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -135,105 +135,96 @@ M.lspconfig = {
       end,
       "LSP declaration",
     },
-
     ["gd"] = {
       function()
         vim.lsp.buf.definition()
       end,
       "LSP definition",
     },
-
+    ["gh"] = { -- or any other key you prefer
+      function()
+        vim.diagnostic.open_float({ border = "rounded" })
+      end,
+      "Show diagnostic in floating window",
+    },
     ["K"] = {
       function()
         vim.lsp.buf.hover()
       end,
       "LSP hover",
     },
-
     ["gi"] = {
       function()
         vim.lsp.buf.implementation()
       end,
       "LSP implementation",
     },
-
     ["<leader>ls"] = {
       function()
         vim.lsp.buf.signature_help()
       end,
       "LSP signature help",
     },
-
     ["<leader>D"] = {
       function()
         vim.lsp.buf.type_definition()
       end,
       "LSP definition type",
     },
-
     ["<leader>ra"] = {
       function()
         require("nvchad.renamer").open()
       end,
       "LSP rename",
     },
-
     ["<leader>ca"] = {
       function()
         vim.lsp.buf.code_action()
       end,
       "LSP code action",
     },
-
-    ["gr"] = {
+    ["<leader>gr"] = {
       function()
-        vim.lsp.buf.references()
+        require('telescope.builtin').lsp_references({ jump_type = 'never', show_line = false, initial_mode = 'normal' })
       end,
-      "LSP references",
+      "Show references in float"
     },
-
     ["<leader>lf"] = {
       function()
         vim.diagnostic.open_float { border = "rounded" }
       end,
       "Floating diagnostic",
     },
-
     ["[d"] = {
       function()
         vim.diagnostic.goto_prev { float = { border = "rounded" } }
       end,
       "Goto prev",
     },
-
     ["]d"] = {
       function()
         vim.diagnostic.goto_next { float = { border = "rounded" } }
       end,
       "Goto next",
     },
-
     ["<leader>q"] = {
       function()
         vim.diagnostic.setloclist()
       end,
       "Diagnostic setloclist",
     },
-
     ["<leader>wa"] = {
       function()
         vim.lsp.buf.add_workspace_folder()
       end,
       "Add workspace folder",
     },
-
     ["<leader>wr"] = {
       function()
         vim.lsp.buf.remove_workspace_folder()
       end,
       "Remove workspace folder",
     },
-
     ["<leader>wl"] = {
       function()
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
@@ -241,7 +232,6 @@ M.lspconfig = {
       "List workspace folders",
     },
   },
-
   v = {
     ["<leader>ca"] = {
       function()
